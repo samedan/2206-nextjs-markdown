c3f28363f727e491b4669206bd507d8232685dba
Daniel Popescu <same.dan@gmail.com>
2022-06-01 11:52:06 +0200

    Added Menu

---
+++ pages/blog/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function BlogPage() {
+  return (
+    <Layout title="">
+      <h1>Blog</h1>
+    </Layout>
+  );
+}
