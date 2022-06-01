7863bff23ec57f1e2094c0eba216d81b6a5c349f
Daniel Popescu <same.dan@gmail.com>
2022-06-01 12:28:28 +0200

    Extract FrontMatter (META) from MD files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
 import Layout from "../components/Layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <Layout>
       <h1>Hello</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const markdowWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    // extracts META from the MD files
+    const { data: frontmatter } = matter(markdowWithMeta);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+  // console.log(posts);
+  return {
+    props: {
+      posts,
+    },
+  };
 }
---
+++ posts/post-one.md
@@ -0,0 +1,10 @@
+---
+title: "Post One"
+author: "Dan Pope"
+---
+
+This is a blog **post**
+
+\*Item
+
+##Heading
