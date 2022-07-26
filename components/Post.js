967638672c21674fe44522f8c8b74eb99b673dd3
Daniel Popescu <same.dan@gmail.com>
2022-07-26 17:10:07 +0200

    First commit

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import CategoryLabel from "./CategoryLabel";
 
-export default function Post({ post }) {
+export default function Post({ post, compact }) {
   // console.log(post);
   // const API = "https://localhost:3000";
   const myLoader = ({ src }) => {
@@ -15,14 +15,17 @@ export default function Post({ post }) {
     return (
       <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
         {/* {post.frontmatter.cover_image} */}
-        <Image
-          loader={myLoader}
-          src={`${post.frontmatter.cover_image}`}
-          alt="image"
-          height="420"
-          width="600"
-          className="mb-4 rounded"
-        />
+        {!compact && (
+          <Image
+            loader={myLoader}
+            src={`${post.frontmatter.cover_image}`}
+            alt="image"
+            height="420"
+            width="600"
+            className="mb-4 rounded"
+          />
+        )}
+
         {/* <img
           // loader={myLoader}
           src={`${post.frontmatter.cover_image}`}
@@ -47,21 +50,23 @@ export default function Post({ post }) {
           <p className="my-2 text-gray-600">{post.frontmatter.excerpt}</p>
         </div>
 
-        <div className="flex justify-between items-center mt-6">
-          <Link href={`/blog/${post.slug}`}>
-            <a className="text-gray-900 hover:text-blue-600">Read more</a>
-          </Link>
-          <div className="flex items-center">
-            <img
-              src={post.frontmatter.author_image}
-              alt="Author"
-              className="mx-4 w-10 object-cover rounded-full hidden sm:block"
-            />
-            <h3 className="text-gray-700 font-bold">
-              {post.frontmatter.author}
-            </h3>
+        {!compact && (
+          <div className="flex justify-between items-center mt-6">
+            <Link href={`/blog/${post.slug}`}>
+              <a className="text-gray-900 hover:text-blue-600">Read more</a>
+            </Link>
+            <div className="flex items-center">
+              <img
+                src={post.frontmatter.author_image}
+                alt="Author"
+                className="mx-4 w-10 object-cover rounded-full hidden sm:block"
+              />
+              <h3 className="text-gray-700 font-bold">
+                {post.frontmatter.author}
+              </h3>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
---
+++ pages/api/search.js
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export default function handler(req, res) {
+  let posts;
+
+  if (process.env.NODE_ENV === "production") {
+    // fetch from cache
+  } else {
+    const files = fs.readdirSync(path.join("posts"));
+    posts = files.map((filename) => {
+      // read the files in the "posts" directory
+      const markdownWithMeta = fs.readFileSync(
+        path.join("posts", filename),
+        "utf-8"
+      );
+      const { data: frontmatter } = matter(markdownWithMeta);
+
+      return {
+        frontmatter,
+      };
+    });
+  }
+
+  const results = posts.filter(
+    ({ frontmatter: { title, excerpt, category } }) =>
+      // check each search term 'q' if it matches (-1)
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.q) != -1
+  );
+  // console.log(results);
+
+  res.status(200).json(results);
+}
